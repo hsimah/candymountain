f396cc02a58a10454681236d15cebc5b09f9d2b1
hsimah <hamishblake+github@gmail.com>
2020-10-03 12:36:52 -0700

    docker

---
+++ candymountain.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: Candy Mountain
+ * Plugin URI: https://polesphere.com
+ * Description: Dashboard plugin for
+ * Author: hsimah, admingemo
+ * Version: 0.10.0-alpha
+ * Text Domain: rhomboid
+ * License: GPL-3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package  CandyMountain
+ * @author   hsimah
+ * @version  0.0.1
+ */
